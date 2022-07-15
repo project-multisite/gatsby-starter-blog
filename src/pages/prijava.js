f69c11d0a5138b17369ef5340de671b7a7a42087
project-multisite <project.multisite@gmail.com>
2022-07-15 09:51:27 +0200

    Zaključno sa stranicom za prijavu

--- src/pages/prijava.js
+++ src/pages/prijava.js
@@ -26,42 +26,10 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <div class="prijava">
-        <Link to="/prijava" class="prijava2">Prijava</Link>
-      </div>
-      <br></br>
-      <Seo title="All posts" />
-      <Bio />
+      <Seo title="Prijava" />
+      
       <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
-
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article">
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
+        
       </ol>
     </Layout>
   )
